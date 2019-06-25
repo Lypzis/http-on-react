90408219ffde90f4b179b799dcc4b6083bd8aa9c
Lypzis <victorvp2008@hotmail.com>
2019-06-25 17:29:46 -0300

    minor update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ class App extends Component {
   render() {
     return (
       //enables React DOM :D, for routing throughout all the app
+      // <BrowserRouter basename="/my-app"> use this whenever the 'app' is in a subfolder,
+      // otherwise defaults to "/"
       <BrowserRouter>
         <div className="App">
           <Blog />
